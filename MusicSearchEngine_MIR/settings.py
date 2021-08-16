863eb2a3650e2995404c27f5dfca0274efdecb9d
zolfaShefreie <z.shefreie@gmail.com>
2021-08-16 11:40:28 +0430

    add env settings

--- MusicSearchEngine_MIR/settings.py
+++ MusicSearchEngine_MIR/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1h4lvjnn8=7k189%82j1zm!wd1gwikx=jgg#s8tbbn)z=d&2c$'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
