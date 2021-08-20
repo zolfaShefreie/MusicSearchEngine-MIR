cbf070534b13c2072963515abeb797528a35c05e
zolfaShefreie <z.shefreie@gmail.com>
2021-08-20 12:58:28 +0430

    SongExplorer

---
+++ mir_sys/indexer/explore_songs.py
@@ -0,0 +1,78 @@
+from ytmusicapi import YTMusic
+from django.conf import settings
+import datetime
+
+from mir_sys.elasticsearch.queries import Queries
+
+
+EXPIRE_YEAR = getattr(settings, 'CHECK_ARTISTS_YEAR', 1)
+
+
+class SongExplorer:
+    ytmusic = YTMusic()
+    queries = Queries()
+    LIMITS = 10
+
+    @staticmethod
+    def get_date_year_ago():
+        return (datetime.datetime.now() - datetime.timedelta(days=EXPIRE_YEAR * 365)).date().strftime("%Y-%m-%d")
+
+    @classmethod
+    def get_artist_videos(cls, artist_id: str) -> list:
+        """
+        :param artist_id:
+        :return: a list of video ids
+        """
+        return_list = list()
+        result = cls.ytmusic.get_artist(artist_id)
+
+        return_list.extend([each['videoId']
+                            for category in [result['songs']['results'], result['videos']['results']]
+                            for each in category])
+        albums = [each['browseId']
+                  for category in [result['singles']['results'], result['albums']['results']]
+                  for each in category]
+        return_list.extend([each for album in albums for each in cls.get_album_videos(album)])
+        return list(set(return_list))
+
+    @classmethod
+    def get_album_videos(cls, album_id: str) -> list:
+        """
+        :param album_id:
+        :return:
+        """
+        results = cls.ytmusic.get_album(album_id)['tracks']
+        return list(set([each['videoId'] for each in results]))
+
+    @classmethod
+    def create_song_objs(cls, ids: list, artist_id: str):
+        """
+        if id doesn't exist create a songs obj
+        :param ids:
+        :param artist_id:
+        :return:
+        """
+        objs = list()
+        exist_objs = cls.queries.exist_ids(ids=ids, index_name="songs")
+        for each in ids:
+            if each not in exist_objs:
+                objs.append({"id": each,
+                             "data": {"artist": artist_id, "seen": False, "fingerprint": ""}})
+        cls.queries.create_multi_objs(objs, "songs")
+
+    @classmethod
+    def update_artist_last_check(cls, artist_id: str):
+        """
+        set last_check to today date
+        :param artist_id:
+        :return:
+        """
+        cls.queries.update_obj(obj_id=artist_id, index_name="artists",
+                               obj={'last_check': datetime.datetime.today().strftime("%Y-%m-%d")})
+
+    @classmethod
+    def run(cls):
+        artists = cls.queries.get_expire_artists(cls.get_date_year_ago(), cls.LIMITS)
+        for each in artists:
+            cls.create_song_objs(ids=cls.get_artist_videos(each))
+            cls.update_artist_last_check(each)
