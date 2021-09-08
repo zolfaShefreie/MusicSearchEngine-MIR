98e56646fd223eaf954e82e1e7b09fbeb421c500
zolfaShefreie <z.shefreie@gmail.com>
2021-09-08 21:23:59 +0430

    add url

--- MusicSearchEngine_MIR/urls.py
+++ MusicSearchEngine_MIR/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from mir_sys.views import SearchFileView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("search/", SearchFileView.as_view()),
 ]
